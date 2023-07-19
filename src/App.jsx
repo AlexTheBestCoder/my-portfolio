a12bc01bff8d7340c1eeff5d32b8182cbfa0640c
AlexTheBestCoder <thebestcoder10@gmail.com>
2023-07-19 14:48:51 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+import Home from "./pages/Home";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
